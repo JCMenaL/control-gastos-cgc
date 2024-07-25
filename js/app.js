0aa5214a9b645481623b826f8f971a3d1cc34f10
jcmsuperstar <coreiconid@gmail.com>
2024-07-24 20:06:20 -0600

    a

--- js/app.js
+++ js/app.js
@@ -3,10 +3,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems);
+
+      // Inicializar el menú lateral
+   var sidenavElems = document.querySelectorAll('.sidenav');
+   var sidenavInstances = M.Sidenav.init(sidenavElems);
+
+   var elems = document.querySelectorAll('.modal');
+   var instances = M.Modal.init(elems);
+   
   });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems);
-  });
-  
+ 
